d769978fe9def0862051e6b06ef578253612b5a1
Francisco Sencion <sfra0480@sa.convergys.com>
2019-07-02 18:33:18 -0400

    redesigned navbar, all content in page now responsive to screen sizes

--- js/main.js
+++ js/main.js
@@ -4,16 +4,15 @@ let tooglebtn = document.querySelector("button")
 let toogle_navbar = function(e) {
   // e.preventDefault()
   if (navbartoogled === false) {
+	document.querySelector('.custom-navbar').style.height = "23rem;"
     document.querySelector('#navbarNav').style.backgroundColor = "#364042";
     // document.querySelector('.mycustom_collapse').style.height = "23rem";
     navbartoogled == true;
-    console.log("btn clicked")
     }
   else if(navbartoogled === true){
+	document.querySelector('.custom-navbar').style.height = "initial;"
     document.querySelector('#navbarNav').style.backgroundColor = "transparent";
-    // document.querySelector('.mycustom_collapse').style.height = "4rem";
     navbartoogled == false;}
-    console.log("btn unclicked")
 }
 
 tooglebtn.addEventListener("click", toogle_navbar, false);
